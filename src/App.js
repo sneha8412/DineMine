ef12ea97193fcd5aee8b793ad57fe8969bb08aa6
sneha agarwal <sneha8412@gmail.com>
2021-08-02 16:21:16 -0700

    deleted App .js Google Auth code after componentization

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
   Redirect
 } from "react-router-dom";
 
-//Pages
+//import Pages
 import HomePage from "./pages"; ///< index.jsx will be automatically imported 
 import SearchResultsPage from './pages/SearchResults';
 import ExperiencePage from './pages/Experience';
@@ -30,113 +30,13 @@ const googleClientId = "153608278319-169t8o4mqbd6lpjhkuqh2lv2n8f2md5r.apps.googl
 
 function App() {
   
-  // const [gapi, setGapi] = useState();
-  // const [googleAuth, setGoogleAuth] = useState();
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [imageUrl, setImageUrl] = useState();
-  
-  // const [newHostFormDisplay, setNewHostFormDisplay] = useState(true)
-
-  // const onSuccess = (googleUser) => {
-  //   setIsLoggedIn(true);
-  //   const profile = googleUser.getBasicProfile();
-  //   setName(profile.getName());
-  //   setEmail(profile.getEmail());
-  //   setImageUrl(profile.getImageUrl());
-  // };
-  
-  // const onFailure = () => {
-  //   setIsLoggedIn(false);
-  // }
-  
-  // const logOut = () => {
-  //   (async() => {
-  //     await googleAuth.signOut();
-  //     setIsLoggedIn(false);
-  //     renderSigninButton(gapi);
-  //   })();
-  // };
-  
-  // const renderSigninButton = (_gapi) => {
-  //   _gapi.signin2.render('google-signin', {
-  //     'scope': 'profile email',
-  //     'width': 240,
-  //     'height': 50,
-  //     'longtitle': true,
-  //     'theme': 'dark',
-  //     'onsuccess': onSuccess,
-  //     'onfailure': onFailure 
-  //   });
-  // }
-  
-  
-  // useEffect(() => {
-    
-  //   //window.gapi is available at this point
-  //   window.onGoogleScriptLoad = () => {
-     
-  //     const _gapi = window.gapi;
-  //     setGapi(_gapi);
-      
-  //     _gapi.load('auth2', () => {
-  //       (async () => { 
-  //         const _googleAuth = await _gapi.auth2.init({
-  //          client_id: googleClientId
-  //         });
-  //         setGoogleAuth(_googleAuth);
-  //         renderSigninButton(_gapi);
-  //       })();
-  //     });
-  //   }
-
-  // //   const onSubmitCallbackForNewHost = (hostFullName, hostAddress, hostPhone, hostIntroduction) => {
-  // //     // call API to create card with provided message
-  // //     axios.post(`${BASE_URL}`, {
-  // //       hostFullName: hostFullName,
-  // //       hostAddress: hostAddress,
-  // //       hostPhone : hostPhone,
-  // //       hostIntroduction: hostIntroduction
-  // //       })
-  // //       .then((response) => {
-  // //         console.log(response.data, '!');
-  // //         // fetchBoards()
-  // //       }, (error) => {
-  // //         console.log(error);
-  // //       });
-  // // };
-  
-    
-  //   //ensure everything is set before loading the script
-  //   loadGoogleScript();
-    
-  // });
-  
-  
   return (
     // home
     
     <div className="App">
       <Header/>
         <GoogleAuth/>
-      {/* <div>
-        {!isLoggedIn &&
-          <div id="google-signin"></div>
-        }
-        
-        {isLoggedIn &&
-          <div className="signed-in-userinfo">
-            <div>
-              <img src={imageUrl} alt="google sign-in"/>
-            </div>
-            <div>{name}</div>
-            <div>{email}</div>
-            <button className='btn-primary' onClick={logOut}>Log Out</button>
-          </div>
-        }
-      </div> */}
-
+    
       {/* banner */}
         {/* search */}
 
