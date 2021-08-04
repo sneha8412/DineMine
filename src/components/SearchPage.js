eb9950ba32744d4756dd05de2594d3698553f80a
sneha agarwal <sneha8412@gmail.com>
2021-08-03 19:18:50 -0700

    changed the image url in SearchPage

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -7,78 +7,77 @@ function SearchPage() {
     return (
         <div className='searchPage'>
             <div className='searchPage__info'>
-                <p>62 stays · 26 august to 30 august · 2 guest</p>
-                <h1>Stays nearby</h1>
+                <p>62 DineMines · 26 august · 2 guest</p>
+                <h1>DineMines nearby</h1>
                 <Button variant="outlined">Cancellation Flexibility</Button>
-                <Button variant="outlined">Type of place</Button>
+                <Button variant="outlined">Type of Cusine</Button>
                 <Button variant="outlined">Price</Button>
-                <Button variant="outlined">Rooms and beds</Button>
+                <Button variant="outlined">Dine Times</Button>
                 <Button variant="outlined">More filters</Button>
             </div>
             <SearchResult
-                img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
-                location="Private room in center of London"
-                title="Stay at this spacious Edwardian House"
-                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                img="https://images.unsplash.com/photo-1573225342350-16731dd9bf3d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=957&q=80"
+                location="Redmond"
+                title="Rustic Farm to Table Dining Experience"
+                description=" Seats 2 guest · Free parking · Outdoor seating · Non-Veg "
                 star={4.73}
-                price="£30 / night"
-                total="£117 total"
+                price="$20 / person"
+                
             />
 
             <SearchResult
-                img="https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg"
-                location="Private room in center of London"
-                title="Independant luxury studio apartment"
-                description="2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen"
+                img="https://images.unsplash.com/photo-1592178036268-cffc32b23b14?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                location="Issaquah"
+                title="The ultimate outdoor grill experience"
+                description="seats 6 guest · Outdoor · Private party . Lunch · Non-Veg "
                 star={4.3}
-                price="£40 / night"
-                total="£157 total"
+                price="$18 / person"
+                
             />
 
             <SearchResult
-                img="https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg"
-                location="Private room in center of London"
-                title="London Studio Apartments"
-                description="4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine"
+                img="https://images.unsplash.com/photo-1530062845289-9109b2c9c868?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80"
+                location="Redmond"
+                title="Vegan delight"
+                description="seats 4 guest ·Vegan buffet · Free parking · Dinner · Vegan · Vegetarian"
                 star={3.8}
-                price="£35 / night"
-                total="£207 total"
+                price="$15 / person"
+                
             />
             <SearchResult
-                img="https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI"
-                location="Private room in center of London"
-                title="30 mins to Oxford Street, Excel London"
-                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                img="https://images.unsplash.com/photo-1608350911676-5975d658264e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80"
+                location="Bellevue"
+                title="Fresh hand-rolled Sushi"
+                description="seats 4 guest · Sushi · Free parking · Non-veg · Dinner"
                 star={4.1}
-                price="£55 / night"
-                total="£320 total"
+                price="$20 / night"
+                
             />
             <SearchResult
-                img="https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg"
-                location="Private room in center of London"
-                title="Spacious Peaceful Modern Bedroom"
-                description="3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning"
+                img="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bHVuY2glMjBpdGFsaWFufGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                location="Seattle"
+                title="American Fries and Burger "
+                description="Seats 6 guest · Vegetarian · Free parking · Non-veg · Lunch and Dinner · Private dining room "
                 star={5.0}
-                price="£60 / night"
-                total="£450 total"
+                price="$12 / night"
+                
             />
             <SearchResult
-                img="https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937"
-                location="Private room in center of London"
-                title="The Blue Room In London"
-                description="2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine"
+                img="https://images.unsplash.com/photo-1600057858461-d15e7dfc6306?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"
+                location="Mercer Island"
+                title="Organic Stone Fired Pizza"
+                description="Seats 10 guest · Outdoor Seating · Vegetarian · Free Parking · Lunch and Dinner"
                 star={4.23}
-                price="£65 / night"
-                total="£480 total"
+                price="$20 / person"
+                
             />
             <SearchResult
-                img="https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp"
-                location="Private room in center of London"
-                title="5 Star Luxury Apartment"
-                description="3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                img="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                location="Seattle"
+                title="Organic Vegetarian Breakfast"
+                description="10 guest · Vegetarian · Vegan · Breakfast · Free parking · Outdoor"
                 star={3.85}
-                price="£90 / night"
-                total="£650 total"
+                price="$10/ person"
             />
         </div>
     )
